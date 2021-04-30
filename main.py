7bcfe5e0f79da4faa7e9e1ea92c7a9ae8c1d92ed
Devin <llamaguy.8267@gmail.com>
2021-04-29 21:24:50 -0400

    Make but bad

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from itertools import tee
+import random
 
 def pairwise(iterable):
     a, b = tee(iterable)
@@ -20,13 +21,48 @@ def generate_dict(userinput):
     
 def generate_text(worddict):
   """
-  generate text
+  generate text using mArKoV cHaIn
   """
 
-def main():
-    usrinput = input("What is your string?: ")
-    worddict = generate_dict(usrinput)
-    print(worddict)
+def generate_bad_text(worddict):
+  length = 10
+  string = []
+  for i in range(length):
+    if i == 0:
+      string.append(random.choice(list(worddict.keys())))
+    else:
+      lastword = string[i-1]
+      string.append(random.choice(list(worddict[lastword].keys())))
+  return string
+
+def main(): 
+  while True:
+    #Im gonna try making the UI like a command line because why not
+    usrinput = input("markov_chain_project: ")
+    usrwords = usrinput.split()
+    if usrwords[0] == "help":
+      print('START    Starts the training process \n [type]                  [file]\ntype in custom text     get text from a file\n\nEXIT     Exits the program')
+    elif usrwords[0] == "start":
+      if usrwords[1] == "type":
+        print('\n')
+        string = input("What is your training input: ")
+        worddict = generate_dict(string)
+        generated = generate_bad_text(worddict)
+        print(generated)
+      elif usrwords[1] == "file":
+        print('\n')
+        fname = input("File name: ")
+        try:
+          with open(fname, "r") as f:
+            text = f.read()
+            f.close()
+          worddict = generate_dict(text)
+          generated = generate_bad_text(worddict)
+          print(generated)
+        except FileNotFoundError:
+          print("File did not exist")
+    elif usrwords[0] == "exit":
+      break
 
 
 if __name__ == "__main__":
